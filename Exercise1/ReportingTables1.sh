4941b3fd47b8c4f6b0e420e83b5a95ea67d1c0c9
root <root@quickstart.cloudera>
2018-06-26 16:26:30 -0700

    hive update

--- Exercise1/ReportingTables1.sh
+++ Exercise1/ReportingTables1.sh
@@ -2,13 +2,13 @@
 
 
 
-hive -e "CREATE  TABLE hive_practical_exercise_1.updates as SELECT a.user_id, a.cnt from(select user_id, count(*) cnt FROM hive_practical_exercise_1.activitylog WHERE type='UPDATE' group by user_id order by user_id)a ;"
+hive -e "CREATE  TABLE hive_practical_exercise_1.updates as select user_id, count(*) cnt FROM hive_practical_exercise_1.activitylog WHERE type='UPDATE' group by user_id ;"
 
 
-hive -e "CREATE  TABLE hive_practical_exercise_1.inserts as SELECT b.user_id, b.InsertCount from (SELECT user_id, count(*) InsertCount FROM hive_practical_exercise_1.activitylog WHERE type='INSERT' group by user_id order by user_id)b ;"
+hive -e "CREATE  TABLE hive_practical_exercise_1.inserts as SELECT user_id, count(*) InsertCount FROM hive_practical_exercise_1.activitylog WHERE type='INSERT' group by user_id ;"
 
 
-hive -e "CREATE  TABLE hive_practical_exercise_1.deletes as SELECT c.user_id,c.deletecount from (SELECT user_id, count(*) deletecount FROM hive_practical_exercise_1.activitylog WHERE type='DELETE' group by user_id order by user_id)c ;"
+hive -e "CREATE  TABLE hive_practical_exercise_1.deletes as SELECT user_id, count(*) deletecount FROM hive_practical_exercise_1.activitylog WHERE type='DELETE' group by user_id ;"
 
 
 hive -e "CREATE  TABLE hive_practical_exercise_1.temp1 as select activitylog.user_id, ts,type from (SELECT user_id,MAX(timestamp) ts from hive_practical_exercise_1.activitylog group by user_id) z left outer join hive_practical_exercise_1.activitylog on z.ts= activitylog.timestamp ;"
@@ -23,10 +23,10 @@ hive -e "CREATE  TABLE hive_practical_exercise_1.isactive as  select ftab.user_i
 #hive -e "CREATE TEMPORARY TABLE hive_practical_exercise_1.isactive as  select ftab.user_id, ftab.s from    (SELECT a.user_id, if  ( unix_timestamp() - ts < 172800, 1,0) s from (select activitylog.user_id, ts from (SELECT user_id,MAX(timestamp) ts from hive_practical_exercise_1.activitylog group by user_id) z left outer join hive_practical_exercise_1.activitylog on z.ts= activitylog.timestamp) a) ftab  ;"
 
 
-hive -e "CREATE  TABLE hive_practical_exercise_1.uploadcount as  select g.user_id, g.upc from (select user_id, count(*)upc from hive_practical_exercise_1.csv group by user_id order by user_id)g ;"
+hive -e "CREATE  TABLE hive_practical_exercise_1.uploadcount as  select user_id, count(*)upc from hive_practical_exercise_1.csv group by user_id  ;"
 
 
-hive -e "CREATE TABLE hive_practical_exercise_1.user_report as select u.id ,upd.cnt, ins.InsertCount,del.deletecount,act.type, isa.s, upl.upc
+hive -e "CREATE TABLE hive_practical_exercise_1.user_report as select u.id ,COALESCE(upd.cnt,0) updatecount, COALESCE(ins.InsertCount,0) insertcount , COALESCE(del.deletecount,0) deletecount ,act.type, isa.s as isactive, COALESCE(upl.upc,0) uploadcount
 from hive_practical_exercise_1.user u
 left outer join hive_practical_exercise_1.updates upd on u.id=upd.user_id
 left outer join hive_practical_exercise_1.inserts ins on u.id=ins.user_id
